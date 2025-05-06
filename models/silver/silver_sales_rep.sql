b1ac4066505d628dbae205dfb5044593209eee26
Black Scorpion <lrgavgo534@tortool.cc>
2025-05-06 14:59:14 +0000

    ss

--- models/silver/silver_sales_rep.sql
+++ models/silver/silver_sales_rep.sql
@@ -2,8 +2,8 @@
 
 with silver_sales_rep AS (
     SELECT
-        'sales_rep_id',
-        'sales_rep_name'
+        sales_rep_id,
+        sales_rep_name
     FROM {{ ref('bronze_sales_data') }}
     GROUP BY 1, 2
 )
